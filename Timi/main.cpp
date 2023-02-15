f45bc4f8130fca0cd7eebb0f7cce2d09d461a932
Mancy Li <limengchen9769@icloud.com>
2023-02-15 15:23:22 +0800

    Initial Commit

---
+++ Timi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Timi
+//
+//  Created by Mengchen Li on 2023/2/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
